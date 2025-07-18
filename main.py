6cea56b794523db3b10f6362d5ba8bd2067c2844
Abdelmawgoud-Shaheen <56496796+Abdelmawgoud-Shaheen@users.noreply.github.com>
2025-07-18 08:49:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+
+API_TOKEN = "8104310460:AAFuNTe_pp722I40CHvgAmwLrkrzOlx2gqQ"
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    web_app = WebAppInfo("https://roastyfaceto.vercel.app/")
+    btn = KeyboardButton(text="🎨 افتح التطبيق", web_app=web_app)
+    markup.add(btn)
+    bot.send_message(message.chat.id, "اضغط الزر لفتح التطبيق:", reply_markup=markup)
+
+bot.polling()
